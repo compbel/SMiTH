dba0dd61a257f6b7d43dba8b65ca4093ba39152e
Computational Biology and Epidemiology Lab <44712151+compbel@users.noreply.github.com>
2024-05-27 09:23:03 -0400

    Update getDiversity.m

--- CODE/getDiversity.m
+++ CODE/getDiversity.m
@@ -1,10 +1,8 @@
 function divers = getDiversity(seqFile,separator,token)
-warning('off', 'all');
 seqF = fastaread(seqFile);
 nSeq = length(seqF);
 nm = char(seqF.Header);
 seq = char(seqF.Sequence);
-seqPat = cell(1,nSeq);
 seqPatID = zeros(1,nSeq);
 L = size(seq,2);
 for i = 1:nSeq
@@ -14,15 +12,22 @@ end
 patientList = sort(unique(seqPatID));
 nPat = length(patientList);
 
+
 maxDist = zeros(1,nPat);
 meanDist = zeros(1,nPat);
 meanEntropy = zeros(1,nPat);
 for i = 1:nPat
     ind = (seqPatID == patientList(i));
     seq_i = seq(ind,:);
-    DM = pdist(seq_i,'hamming');
-    maxDist(i) = max(DM(:));
-    meanDist(i) = mean(DM(:));
+    
+    if sum(ind) > 1
+        DM = pdist(seq_i, 'hamming');
+        maxDist(i) = max(DM(:));
+        meanDist(i) = mean(DM(:));
+    else
+        maxDist(i) = 0;
+        meanDist(i) = 0;
+    end
 
     totalEntropy = 0;
     for col = 1:L
