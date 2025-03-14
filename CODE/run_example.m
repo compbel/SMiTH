3549d2e13559370a3d2dfb881a06c5dfba1feb3d
Computational Biology and Epidemiology Lab <44712151+compbel@users.noreply.github.com>
2025-03-14 19:59:01 -0400

    Add files via upload

---
+++ CODE/run_example.m
@@ -0,0 +1,17 @@
+clear;
+filePhylo = ['input example' filesep 'tree16.csv'];
+sampGenerator = @randTreePrefAttach;
+nSamp = 10000;
+constr = 'convexMaxCompact'; 
+fileSeq = ['input example' filesep 'sequence_data16.fasta'];
+delimeter = '|';
+tokenPos = 2;
+timeLimit = 600;
+
+[migrSamp,objSamp,originSamp,consensus, siteList] = migrationSampler(filePhylo,sampGenerator,...
+    nSamp,constr,timeLimit,fileSeq,delimeter,tokenPos);
+
+% In these lines, we constract a maximal spanning tree of the consensus,
+% and plot it
+migrSpan = minspantree(graph(-consensus));
+plot(migrSpan,'Layout','force','NodeLabel',string(siteList));
